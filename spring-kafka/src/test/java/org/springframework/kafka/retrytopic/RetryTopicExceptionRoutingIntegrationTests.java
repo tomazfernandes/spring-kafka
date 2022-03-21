699414c6e91662144af3afb1113d93385f037a40
Tomaz Fernandes <tomaz.fernandes.se@gmail.com>
2022-03-21 18:45:22 -0300

    Fix test deprecation warning

--- spring-kafka/src/test/java/org/springframework/kafka/retrytopic/RetryTopicExceptionRoutingIntegrationTests.java
+++ spring-kafka/src/test/java/org/springframework/kafka/retrytopic/RetryTopicExceptionRoutingIntegrationTests.java
@@ -37,7 +37,6 @@ import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.DltHandler;
@@ -395,10 +394,8 @@ public class RetryTopicExceptionRoutingIntegrationTests {
 		}
 
 		@Bean(name = RetryTopicInternalBeanNames.DESTINATION_TOPIC_CONTAINER_NAME)
-		public DefaultDestinationTopicResolver ddtr(ApplicationContext applicationContext,
-													@Qualifier(RetryTopicInternalBeanNames
-															.INTERNAL_BACKOFF_CLOCK_BEAN_NAME) Clock clock) {
-			DefaultDestinationTopicResolver ddtr = new DefaultDestinationTopicResolver(clock, applicationContext);
+		public DefaultDestinationTopicResolver ddtr() {
+			DefaultDestinationTopicResolver ddtr = new DefaultDestinationTopicResolver();
 			ddtr.addNotRetryableExceptions(ShouldSkipBothRetriesException.class);
 			return ddtr;
 		}
